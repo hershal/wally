f83f7f7dcaad6f953a8e8991fb4e848d49c4e54b
Hershal Bhave <hershal.bhave@gmail.com>
2015-12-28 00:48:08 -0600

    Initial Commit

---
+++ test/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  test
+//
+//  Created by Hershal Bhave on 12/28/15.
+//  Copyright © 2015 Hershal Bhave. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
